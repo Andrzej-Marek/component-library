c9231e3473211ade9b06620465ea6cd3547fdb99
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2023-12-14 19:14:02 +0100

    workflow to publish package

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "my-library",
+      name: "component-library",
       fileName: "index",
     },
     rollupOptions: {
